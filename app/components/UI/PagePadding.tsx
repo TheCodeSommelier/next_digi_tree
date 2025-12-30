65882af563e4a3223ac9bd09df4e4815527e8c77
Tony Masek <tm@tony-masek.com>
2025-12-30 11:00:24 +0100

    feat: add responsivness to pagePadding

--- app/components/UI/PagePadding.tsx
+++ app/components/UI/PagePadding.tsx
@@ -1,7 +1,7 @@
 import { FC, PropsWithChildren } from 'react';
 
 const PagePadding: FC<PropsWithChildren> = ({children}) => {
-  return <div className="px-32">{children}</div>;
+  return <div className="px-8 lg:px-32">{children}</div>;
 };
 
 export default PagePadding;
