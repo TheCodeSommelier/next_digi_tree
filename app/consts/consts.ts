fee22af48115a1ade7a49eb9f123e6aa3f7de6f4
Tony Masek <tm@tony-masek.com>
2026-01-02 23:45:26 +0100

    feat: add manifest, sitemap and delete unused icon

--- app/consts/consts.ts
+++ app/consts/consts.ts
@@ -5,3 +5,6 @@ export const contacts = {
   companyName: 'DIGITREE EDUCATION',
   address: 'U Hodin 122, 110 00 Praha'
 } as const;
+
+export const isProduction = process.env.NODE_ENV === 'production';
+export const BASE_URL = !isProduction ? 'http://localhost:3000' : 'https://www.digitree-edu.com';
