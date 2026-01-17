074bb582f907aa28f32e306a4f83c23b68668447
Tony Masek <tm@tony-masek.com>
2026-01-17 10:09:34 +0100

    feat: add favicon and apple touch icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body
         className={`${figtree.variable} pt-20 antialiased`}
       >
