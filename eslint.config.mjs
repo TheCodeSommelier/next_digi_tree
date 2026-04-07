3f892c7c4222c095d4a1df30de3b62c254cd38ef
Tony Masek <tm@tony-masek.com>
2026-04-07 15:46:51 +0200

    chore: eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTs from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   {
     rules: {
-      'max-len': [
-        'error',
+      "max-len": [
+        "error",
         {
           code: 120,
           ignoreStrings: true,
@@ -16,23 +16,22 @@ const eslintConfig = defineConfig([
           ignoreComments: true,
         },
       ],
-      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-      semi: ['error', 'always'],
+      semi: ["error", "always"],
       // JSX formatting via ESLint
-      'react/jsx-indent': ['error', 2],
-      'react/jsx-indent-props': ['error', 2],
-      'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
-      'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
-      'no-console': ['error', { allow: ['warn', 'error'] }],
+      "react/jsx-indent": ["error", 2],
+      "react/jsx-indent-props": ["error", 2],
+      "react/jsx-closing-bracket-location": ["error", "tag-aligned"],
+      "react/jsx-first-prop-new-line": ["error", "multiline-multiprop"],
+      "no-console": ["error", { allow: ["warn", "error"] }],
     },
   },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
   ]),
 ]);
 
